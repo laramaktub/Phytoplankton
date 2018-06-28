58c25879f7488c673331c45de7d380f7c08b9703
laramaktub <lara.cern@gmail.com>
2018-06-28 15:51:41 +0000

    first commit

---
+++ scriptmagico.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+try:
+	os.remove("synsets.txt")
+	os.remove("train.txt")
+except OSError:
+	pass
+
+j=0
+for base, dirs, files in os.walk('.'):
+	for d in dirs[:]:
+		
+		d_sin_barras=d.replace("_"," ")
+		with open("synsets.txt", "a") as myfile:
+    			myfile.write(d_sin_barras+'\n')
+		for i in os.listdir(d):
+			with open("train.txt", "a") as afile:
+				afile.write(d + "/" + os.path.relpath(i) + " " + str(j) + '\n')
+		j=j+1
